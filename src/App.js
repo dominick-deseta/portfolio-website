4a65f5e0faf23c37d3d4e28005cf5b9460fd2317
Dominick DeSeta <70176540+dominick-deseta@users.noreply.github.com>
2023-11-02 21:26:39 -0500

    Edited tab icon and title

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 import headshot from './resources/dom_headshot.jpg'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 function App() {
   return (
     <div className="App">
